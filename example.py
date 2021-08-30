e758e47dd8a14981e636b37c17a0fc23083fb68d
ChefJ <hiecreo@gmail.com>
2021-08-30 16:41:37 +0800

    Update example.py
    增加获取rawkey的函数

--- example.py
+++ example.py
@@ -3,6 +3,9 @@
 #pip install pycryptodome
 import hashlib
 from Crypto.Hash import keccak
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+
 
 def decrypt_key(pwd, kdfparams):
     #解密的过程调这个script就行
@@ -20,6 +23,15 @@ def verify_key(dec_key, ciphertext, mac):
         return True
     return False
 
+
+def get_raw_key(dec_key, iv, ciphertext):
+    iv_int=int(iv, 16)
+    ctr = Counter.new(AES.block_size * 8, initial_value=iv_int)
+    dec_suite = AES.new(dec_key, AES.MODE_CTR, counter=ctr)
+    plain_key = dec_suite.decrypt(bytes.fromhex(ciphertext))
+    return plain_key
+
+    
 kdfparams={
          "dklen":32,
          "n":65536,
@@ -30,9 +42,11 @@ kdfparams={
 pwd = '密码？'
 ciphertext = "2c9ed7199f4d54871187e681133f41a3c8b064a76131465cbcd3db6ec5c3a29d"
 mac = "f3eb4036ccd10387dc71178a4cde4fc0612924f97d3f5a52b4f7b9cbcaea0617"
+iv="0e31facc70a4d98c1f5d3a0226ccdb34"
 
 my_key = decrypt_key(pwd, kdfparams)
 if verify_key(my_key, ciphertext, mac):
     print('BINGO!恭喜恭喜。')
+    raw_key = get_raw_key(my_key, iv, ciphertext)
 else:
     print('WRONG.再试试吧。')
